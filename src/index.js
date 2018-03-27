0738f23d8f97b30512db2779ecf56921131d198c
ccfine <469781947@qq.com>
2018-03-28 00:44:55 +0800

    recruit-app-react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./redux/reducer.js";
 import registerServiceWorker from "./registerServiceWorker";
+
+let store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension? window.devToolsExtension(): f => f
+));
 
 // ReactDom.render(
 //   ,
