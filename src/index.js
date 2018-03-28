86f08f70c8f218cd82e9e3f1001ded8902aeeef6
ccfine <469781947@qq.com>
2018-03-28 18:46:22 +0800

    recruit-app-react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 import reducer from "./redux/reducer.js";
 import registerServiceWorker from "./registerServiceWorker";
 
@@ -10,8 +12,12 @@ let store = createStore(reducer, compose(
   window.devToolsExtension? window.devToolsExtension(): f => f
 ));
 
-// ReactDom.render(
-//   ,
-//   document.getElementById("root")
-// );
+ReactDom.render(
+  <Provider store={store}>
+    <BrowserRouter>
+    </BrowserRouter>
+  </Provider>
+  ,
+  document.getElementById("root")
+);
 registerServiceWorker();
